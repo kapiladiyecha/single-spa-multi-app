117f5bdbe7368d4d61d635757ff84ac52c4152a2
kapiladiyecha <kapiladiyecha@gmail.com>
2020-07-14 02:58:34 +0800

    Single-spa multi-app POC

---
+++ app1/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { assetUrl } from 'src/single-spa/asset-url';
+
+@Component({
+  selector: 'app1-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app1';
+  yoshiUrl = assetUrl("yoshi.png");
+  columnDefs = [
+    {field: 'make' },
+    {field: 'model' },
+    {field: 'price'}
+  ];
+
+  rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 }
+  ];
+}
---
+++ app1/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import {AgGridModule} from 'ag-grid-angular';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmptyRouteComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AgGridModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
